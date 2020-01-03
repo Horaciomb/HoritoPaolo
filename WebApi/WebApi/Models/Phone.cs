93cd48e8800932252e2fade7760bffa9f75a647d
visual_community@outlook.com <visual_community@outlook.com>
2020-01-03 16:49:39 -0400

    Pausa porque esta compu de mierda no tiene sqlllll

---
+++ WebApi/WebApi/Models/Phone.cs
@@ -0,0 +1,26 @@
+﻿
+namespace WebApi.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum TypeContact
+    {
+        PhoneNumber,
+        Email,
+        Facebook,
+        Twitter,
+        LinkeDin,
+    }
+
+    public class Phone
+    {
+        [Key]
+        public int PhoneID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public TypeContact Type { get; set; }
+        [Required]
+        public string Contact { get; set; }
+    }
+}
